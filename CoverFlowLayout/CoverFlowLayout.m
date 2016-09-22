0841c3d5109d0e4e013e851969a55814817a50e2
philipycha <philip.yc.ha@gmail.com>
2016-09-22 14:50:32 -0700

    got it to enlarge

--- CoverFlowLayout/CoverFlowLayout.m
+++ CoverFlowLayout/CoverFlowLayout.m
@@ -13,7 +13,8 @@
 - (void)prepareLayout{
     
     self.scrollDirection = UICollectionViewScrollDirectionHorizontal;
-    self.itemSize = CGSizeMake(250, 250);
+    self.sectionInset = UIEdgeInsetsMake(150, 150, 150, 150);
+    self.itemSize = CGSizeMake(100, 100);
     
 }
 
@@ -29,6 +30,21 @@
     for (UICollectionViewLayoutAttributes *attribute in attributes){
         
         if(CGRectIntersectsRect(rect, attribute.frame)){
+            
+            float distanceFromOrigin = (attribute.center.x - (visibleRegion.origin.x + visibleRegion.size.width / 2));
+            
+            if (fabs(distanceFromOrigin) < 80){
+                
+                attribute.transform3D = CATransform3DMakeScale(2, 2, 0);
+                
+                // frame moves at the same time so you have to cancel it out by distancefromorigin / 2
+                
+                attribute.transform3D = CATransform3DTranslate(attribute.transform3D, -distanceFromOrigin/2, 0, 0);
+
+                
+                
+            }
+            
             
             
             
